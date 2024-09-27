8d23f1478cfd3aa4b80360b970302f409f5d8bbf
Nishant Ranout <nishant.ranaut9@gmail.com>
2024-09-27 15:31:04 +0530

    Changing the bucket name.

--- iac/terraform/storage/main.tf
+++ iac/terraform/storage/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "terraform-bucket" {
-  name          = "nishant-25121994"
+  name          = "nishant-2512"
   location      = var.region
   storage_class = "STANDARD"
 }
